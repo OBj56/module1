1a1322d45451e0d543f598b9f764f8ae32d4fd63
musa islam <hamerofth@gmail.com>
2024-05-14 20:02:20 -0400

    21

---
+++ PersonalInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class PersonalInfo
+{
+    static void Main()
+    {
+        string name1 = "Lucinda Potter";
+        DateTime birthdate1 = new DateTime(1992, 6, 24);
+        string workPhoneNumber1 = "000-000-0101";
+        string cellPhoneNumber1 = "000-000-0189";
+
+        Console.WriteLine(name1);
+        Console.WriteLine(birthdate1.ToShortDateString());
+        Console.WriteLine("work " + workPhoneNumber1);
+        Console.WriteLine("cell " + cellPhoneNumber1);
+    }
+
+
+
+
+}
