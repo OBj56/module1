c2b418e01c24ada16ef72dac206f36973e144df7
musa islam <hamerofth@gmail.com>
2024-05-25 18:43:10 -0400

    added /n

---
+++ Lyrics/Lyrics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+ class Lyrics
+{
+    static void Main(string[] args)
+    {
+      var a = "Oh, well imagine /\n" +
+                        "As I'm pacing the pews in a church corridor /\n" +
+                        "And I can't help but to hear /\n" +
+                        "No, I can't help but to hear an exchanging of words...";
+
+        WriteLine(a);
+
+
+    }
+
+
+}
